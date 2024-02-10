45b4dc3a2515cd5694a9e3dd05fad47844139091
D.B.S.S Dassanayake <bhanukadassanayake@gmail.com>
2024-02-11 01:52:59 +0530

    Creating FD Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import FDetialPage from "./pages/FDetialPage";
 export default function App() {
-  return <h1 className="text-4xl  font-bold underline ">Hello world!</h1>;
+  return <FDetialPage />;
 }
