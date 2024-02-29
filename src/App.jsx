c580fa6f12aa1f47a054e22693ec01cd19258e7f
D.B.S.S Dassanayake <bhanukadassanayake@gmail.com>
2024-03-01 03:08:22 +0530

    Minor update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { NavBar } from "./components/NavBar";
 import Container from "./components/Container";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import FDetialPage from "./pages/FDetialPage";
 import MapViewPage from "./pages/MapViewPage";
 
 export default function App() {
-  return <FDetialPage />;
+  return (
+    <Routes>
+      <Route path="/details" element={<FDetialPage />} />
+      <Route path="/map" element={<MapViewPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
