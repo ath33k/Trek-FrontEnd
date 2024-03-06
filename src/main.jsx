482c5b23a12339ff4a40b20a3f06ab114664c7cb
D.B.S.S Dassanayake <bhanukadassanayake@gmail.com>
2024-03-06 22:04:50 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { storeR } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      <Provider store={storeR}>
         <App />
       </Provider>
     </BrowserRouter>
