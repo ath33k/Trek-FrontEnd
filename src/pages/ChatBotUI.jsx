3f6087356615f43a1b6ef2ef1e4273eb2ed5a295
D.B.S.S Dassanayake <bhanukadassanayake@gmail.com>
2024-03-25 10:56:02 +0530

    HTTPS testing 2

--- src/pages/ChatBotUI.jsx
+++ src/pages/ChatBotUI.jsx
@@ -27,13 +27,16 @@ const ChatBotUI = () => {
       return;
     }
 
-    fetch("https://13.232.155.126/generate_tags", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ sentence: inputValue }),
-    }).then((response) => {
+    fetch(
+      "https://ec2-13-232-155-126.ap-south-1.compute.amazonaws.com/generate_tags",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ sentence: inputValue }),
+      }
+    ).then((response) => {
       response
         .json()
         .then((spots) => {
